ec0b62d0abbc30ead5bee546f6b59d78ae677cab
GabrielGabo34 <88508331+GabrielGabo34@users.noreply.github.com>
2022-06-26 15:44:12 -0500

    Base de datos en Clever Cloud

--- include/database.php
+++ include/database.php
@@ -1,8 +1,12 @@
 <?php
-    $servername = 'localhost';
+    /*$servername = 'localhost';
     $username = 'root';
     $password = 'Jose3898';
-    $dbname = 'barespe';
+    $dbname = 'barespe';*/
+    $servername = 'b54bptnsb4k4clstxpmi-mysql.services.clever-cloud.com';
+    $username = 'ubxbizb5j5juf5wq';
+    $password = 'vtPyxU7jgbJmTGC4RPKy';
+    $dbname = 'b54bptnsb4k4clstxpmi';
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
